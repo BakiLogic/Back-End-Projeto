d52e031a863a13acca3220034ae79c0d06b2d44a
BakiLogic <thiagotagami@gmail.com>
2023-12-08 19:09:30 -0300

    coisas

--- src/routes/install.js
+++ src/routes/install.js
@@ -1,2 +1,33 @@
 const express = require('express')
 const router = express.Router()
+
+const preLoad = require("../init/startingData")
+const User = require('../model/user')
+const Categoria = require('../model/categoria')
+const Receita = require("../model/receita")
+const Ingrediente = require("../model/ingrediente")
+
+
+router.get('/install', async (req, res) => {
+
+    await User.sync({ force: true })
+    await Categoria.sync({ force: true })
+    await Receita.sync({ force: true })
+    await Ingrediente.sync({ force: true })
+
+    for (var count = 0; count < preLoad.users.length; count++) {
+        User.create(preLoad.users[count])
+    }
+    for (count = 0; i < preLoad.categorias.length; count++) {
+        Categoria.create(preLoad.categorias[count])
+    }
+    for (count = 0; count < preLoad.receitas.length; count++) {
+        Receita.create(preLoad.receitas[count])
+    }
+    for (count = 0; count < preLoad.ingredientes.length; count++) {
+        Ingrediente.create(preLoad.ingredientes[count])
+    }
+
+    res.json({msg:"Sucesso ao inicializar os dados"})
+
+})
