4d5d88d205cd68ee6921fbd050916c9a01bb55fe
BakiLogic <thiagotagami@gmail.com>
2023-12-10 21:49:14 -0300

    sei lá

--- src/routes/API/menu.js
+++ src/routes/API/menu.js
@@ -1,37 +1,29 @@
 const express = require('express')
 const router = express.Router()
-const jwt = require('jsonwebtoken')
 const Receita = require('../../functions/receitasFunc')
-const Ingrediente = require('../../functions/igFunc')
+const ingredienteList = require('../../model/ingrediente')
+
 const {checkLogged} = require('../../middleware/checkLog')
 
 
-router.post('/menu', checkLogged, async (req, res) => {
-   let checkDo = true
-    do{
+router.get('/menu/:id', checkLogged, async (req, res) => {
+   
+   
         let listaReceitas
-        let ingredientes
         let tempoTotal
-        let check = await Receita.buscarPorId(req.body)
+        let check = await Receita.searchId(req.params.id)
 
         if (check) {
-            listaReceitas = listaReceitas + ',\n' + check.nome
-            let checkIng = await Ingrediente.findAll({where: {idReceita: check.id} })
-            if (checkIng){
-                ingredientes = ingredientes +  ' , ' + String(checkIng.nome)
-            }
+           listaReceitas = listaReceitas + ',\n' + check.nome
+            let ingredientes = await ingredienteList.findAll({where: {idReceita: check.id} })
             tempoTotal = tempoTotal + check.tdp
-            res.json({listaReceitas, msg: ", \n ", ingredientes,msg: ", \n", tempoTotal })         
+           res.json({listaReceitas, msg: ", \n ", ingredientes, msg: ", \n", tempoTotal })         
         } else {
         res.json({msg: "não encontrado"})
-        if(req.body < 0){
-            checkDo = false
-        }
-        } 
+        
+      } 
 
-
-
-    }while (checkDo)
+    
 
 })
 
